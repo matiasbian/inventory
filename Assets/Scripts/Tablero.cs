54f8674210dd0eac10724a9ef0816424f53cc844
Matias Bianchi <matiasezequielbian@gmail.com>
2017-12-19 18:42:44 -0300

    Comienzo de script de tablero, comandos basicos

---
+++ Assets/Scripts/Tablero.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tablero : MonoBehaviour {
+	public int lenghtX,lenghtY;
+	public static Vector2Int position;
+	public static int[,] board;
+	// Use this for initialization
+	void Start () {
+		board = new int[lenghtX, lenghtY];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			moveTo (5, 5);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			Debug.Log (getPosition ());
+			Debug.Log (getBoardLenght ());
+		}
+	}
+
+	public static Vector2Int getPosition(){
+		return position;
+	}
+
+	public static Vector2Int getBoardLenght(){
+		return new Vector2Int(board.GetLength (0),board.GetLength(1));
+	}
+
+	public void moveTo(int X, int Y){
+		if (between (X, 0, lenghtX - 1) && between (Y, 0, lenghtY - 1)) {
+			position = new Vector2Int (X, Y);
+		} else {
+			Debug.LogError ("out of board");
+		}
+	}
+
+
+	#region auxiliares
+	bool between(int valor,int min, int max){
+		return valor >= min && valor <= max;
+	}
+
+	#endregion}
+
+}
